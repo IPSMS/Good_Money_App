c5d23d77cb90acfd95eeb78ff48b04c5c950b86d
Bobby BuffaloBoy <novenarii@gmail.com>
2020-08-26 19:27:29 -0400

    Added username attr to User model

--- GM_API/db/schema.rb
+++ GM_API/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_02_201338) do
+ActiveRecord::Schema.define(version: 2020_08_26_231947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2020_08_02_201338) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
   end
 
   add_foreign_key "action_infos", "stats"
