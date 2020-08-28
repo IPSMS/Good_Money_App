be44076339e3416542c2fe459ee431c31afb854d
tronicdata <larrybuffaloboy@gmail.com>
2020-08-27 23:46:08 -0400

    Styled the login page, changed the secondary theme to green. Fixed the routing

--- GM_Frontend/src/pages/Login.js
+++ GM_Frontend/src/pages/Login.js
@@ -1,12 +1,13 @@
 import React from "react";
 import AppBar from "../components/AppBar";
 import CompleteButton from "../components/CompleteButton";
+import {Link} from "react-router-dom";
 
 // To create the styles object
 import { makeStyles } from "@material-ui/core/styles";
 
 // Material UI imports
-import { Grid, Container, Typography, TextField, Button } from "@material-ui/core";
+import { Grid, Container, Typography, TextField, Button, Box } from "@material-ui/core";
 
 // Styles Object
 const useStyles = makeStyles((theme) => ({
@@ -22,6 +23,30 @@ const useStyles = makeStyles((theme) => ({
   grandTotalBox: {
     textAlign: 'right',
     display: 'inline-block'
+  },
+  formInputs__submit: {
+    marginTop: "1rem",
+    color: "#fff",
+    marginTop: "1rem",
+    fontWeight: "bold"
+  },
+  signupContainer: {
+    marginTop: "2rem"
+  },
+  signup_button: {
+    textAlign: "center", 
+    marginTop: "1rem",
+    fontWeight: "bold"
+  },
+  textDecorationNone: {
+      textDecoration: "none"
+  },
+  headerTitle: {
+      marginBottom: "2rem",
+      fontWeight: "bold"
+  },
+  wrapper: {
+    paddingTop: "3rem"
   }
 }));
 
@@ -30,44 +55,59 @@ export default function Login() {
   const classes = useStyles();
 
   return (
-    <Container maxWidth="sm">
-        <Typography variant="h1" >
-            Login:
+    <Container maxWidth="sm" className={classes.wrapper}>
+        <Typography variant="h3" className={classes.headerTitle}>
+            LOGIN:
         </Typography>
-        <TextField
-          id="standard-full-width"
-          label="Username:"
-          style={{ margin: 8 }}
-          placeholder="Username"
-          helperText="Full width!"
-          fullWidth
-          margin="normal"
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
-        <TextField
-          id="standard-full-width"
-          label="Password:"
-          style={{ margin: 8 }}
-          placeholder="Password"
-          helperText="Full width!"
-          fullWidth
-          margin="normal"
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
-        <Button variant="contained" color="secondary">
-            SUBMIT
-        </Button>
+        <Box className="formInputs" >
+            <TextField
+            id="standard-full-width"
+            label="Username:"
+            style={{ margin: 8 }}
+            placeholder="Username"
+            fullWidth
+            margin="normal"
+            InputLabelProps={{
+                shrink: true,
+            }}
+            />
+            <TextField
+            id="standard-full-width"
+            label="Password:"
+            style={{ margin: 8 }}
+            placeholder="Password"
+            fullWidth
+            margin="normal"
+            InputLabelProps={{
+                shrink: true,
+            }}
+            />
+            <Button 
+                variant="contained" 
+                color="secondary" 
+                className={classes.formInputs__submit} 
+                size="large" 
+                onClick={()=>{ console.log('WINNER!')}}
+            >
+                SUBMIT
+            </Button>                                       
+        </Box>
+        
+        <Box className={classes.signupContainer}>
+            <Typography variant="p" align="center" display="block" >
+                New to GoodMoney? Signup Below!
+            </Typography>
+            <Box className={classes.signup_button}>
+                <Link
+                    to="/signup"
+                    className={classes.textDecorationNone}
+                    >
+                    <Button variant="outlined" color="secondary" size="large">SIGN UP</Button>
+                </Link>
+            </Box>
+            
+        </Box>
 
-        <Typography variant="p" >
-            New to GoodMoney? Signup Below!
-        </Typography>
-        <Button variant="outlined" color="secondary">
-            SIGN UP
-        </Button>
     </Container>
   );
 }
